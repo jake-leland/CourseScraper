3f9f245982ed9f4bddcda3f6ab0e20ea50c2f660
Jake Leland <jake.leland@att.net>
2017-02-17 11:38:28 -0600

    Reworked a huge amount of things. Moved the contents of CourseListingScraper to a new file called SectionListingScraper. This is no longer used, because it is automatically handled by the web app. Reworked CourseListingScraper to get only the listing of the courses, but not any information about the sections. This information is POSTed to the web app, and the app will automatically fill in the section details. Starting work on GradeHistoryScraper, to get grade distribution information from the registrar.

--- src/main/java/tools/CourseListingScraper.java
+++ src/main/java/tools/CourseListingScraper.java
@@ -1,63 +1,88 @@
 package tools;
 
-import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.*;
 import com.gargoylesoftware.htmlunit.html.*;
+import com.gargoylesoftware.htmlunit.util.NameValuePair;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.FileWriter;
+import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class CourseListingScraper {
     public static void main(String[] args) throws Exception {
+        Scanner kb = new Scanner(System.in);
+
         try (final WebClient webClient = new WebClient()) {
             webClient.getOptions().setThrowExceptionOnFailingStatusCode(false); // because Howdy has problems
             webClient.getOptions().setThrowExceptionOnScriptError(false); // because Howdy has problems
 
-            //JSONObject tamuJson = new JSONObject();
+            JSONArray courses = new JSONArray();
             JSONArray subjects = new JSONArray();
 
-            // Howdy Login
-            HtmlPage page = webClient.getPage("https://howdy.tamu.edu/");
-
             // CAS
-            page = (HtmlPage) page.executeJavaScript("casRedirect();").getNewPage();
+            System.out.println("CAS");
+            HtmlPage page = webClient.getPage("https://cas.tamu.edu/cas/login?service=https://compass-sso.tamu.edu:443/ssomanager/c/SSB?pkg=bwykfcls.p_sel_crse_search;renew=true");
             HtmlForm login = (HtmlForm) page.getElementById("fm1");
             login.getInputByName("username").setValueAttribute(args[0]);
             login.getInputByName("password").setValueAttribute(args[1]);
 
-            // Howdy Home
+            // Search Class Schedule
+            System.out.println("Search Class Schedule");
             page = login.getButtonsByName("").get(0).click();
 
-            // My Record
-            page = page.getAnchorByText("My Record").click();
-
-            // Search Class Schedule
-            page = page.getAnchorByText("Search Class Schedule").click();
-
-            // (isolate frame)
-            page = webClient.getPage(page.getFrameByName("content").getEnclosedPage().getUrl());
-
             // Select Term
-            page = page.getForms().get(1).getInputByValue("Submit").click();
+            HtmlForm termForm = page.getForms().get(1);
+            HtmlSelect termSelect = termForm.getSelectByName("p_term");
+            System.out.println("\nSELECT TERM:");
+            for (int i = 0; i < termSelect.getOptions().size(); i++) {
+                HtmlOption t = termSelect.getOption(i);
+                System.out.println("[" + i + "]" + t.getText() + " " + t.getValueAttribute());
+            }
+            int choice = kb.nextInt();
+            HtmlOption currentTerm = termSelect.getOption(choice);
+            String term = currentTerm.getValueAttribute();
+            String termTitle = currentTerm.getText();
+
+            WebRequest requestSettings1 = new WebRequest(
+                    new URL("https://aggie-scheduler.mybluemix.net/api/term"), HttpMethod.POST);
+            requestSettings1.setRequestParameters(new ArrayList());
+            requestSettings1.getRequestParameters().add(new NameValuePair("term", term));
+            requestSettings1.getRequestParameters().add(new NameValuePair("name", termTitle));
+            Page page1 = webClient.getPage(requestSettings1);
+
+            page = termForm.getInputByValue("Submit").click();
+
+            // Select Subject
             HtmlForm subjectsForm = page.getForms().get(1);
             HtmlSelect subjectsSelect = subjectsForm.getSelectByName("sel_subj");
 
             for (HtmlOption subjectOption : subjectsSelect.getOptions()) {
-                subjectsSelect.setSelectedIndex(subjectOption.getIndex() - 1);
+                subjectsSelect.setSelectedIndex(subjectOption.getIndex() / 2);
 
-                JSONObject subjectJson = new JSONObject();
-
-                JSONObject coursesJson = new JSONObject();
+                JSONArray specificCourses = new JSONArray();
 
                 // Specific Subject
                 final HtmlPage subjectPage = subjectsForm.getInputByValue("Course Search ").click();
-                System.out.println(subjectOption.getText());
-                HtmlTable subjectTable = (HtmlTable) subjectPage.getBody().getElementsByAttribute("table", "class", "datadisplaytable").get(0);
+
+                JSONObject subjectJson = new JSONObject();
 
                 String subject = subjectOption.getValueAttribute();
+                String subjectTitle = subjectOption.getText();
+
+                subjectJson.put("_id", subject);
+                subjectJson.put("subject", subject);
+                subjectJson.put("title", subjectTitle);
+
+                subjects.put(subjectJson);
+
+                System.out.println(subjectTitle);
+                HtmlTable subjectTable = (HtmlTable) subjectPage.getBody().getElementsByAttribute("table", "class", "datadisplaytable").get(0);
 
                 boolean skip = true;
                 for (HtmlTableRow courseRow : subjectTable.getRows()) {
@@ -68,81 +93,41 @@ public class CourseListingScraper {
 
                     JSONObject courseJson = new JSONObject();
 
-                    HtmlForm courseForm = (HtmlForm) courseRow.getCells().get(3).getHtmlElementsByTagName("form").get(0);
+                    String course = courseRow.getCells().get(1).getTextContent();
+                    String courseTitle = courseRow.getCells().get(2).getTextContent();
 
-                    JSONObject sectionsJson = new JSONObject();
+                    courseJson.put("_id", subject + " " + course);
+                    courseJson.put("subject", subject);
+                    courseJson.put("course", course);
+                    courseJson.put("title", courseTitle);
 
-                    // Specific section
-                    final HtmlPage sectionPage = courseForm.getInputByValue("View Sections").click();
-                    HtmlTable sectionTable = (HtmlTable) sectionPage.getBody().getElementsByAttribute("table", "class", "datadisplaytable").get(0);
-
-                    boolean skip1 = true;
-                    boolean skip2 = true;
-
-                    String section = "";
-                    JSONObject sectionJson = new JSONObject();
-                    int classes = 0;
-                    for (HtmlTableRow sectionRow : sectionTable.getRows()) {
-                        if (skip1) {
-                            skip1 = false;
-                            continue;
-                        } // skip first row
-                        if (skip2) {
-                            skip2 = false;
-                            continue;
-                        } // skip second row
-
-                        List<HtmlTableCell> sectionCells = sectionRow.getCells();
-
-                        if (!sectionCells.get(1).getTextContent().equals(" ")) {
-                            //String subject = sectionCells.get(2).getTextContent();
-                            //String course = sectionCells.get(3).getTextContent();
-
-                            section = sectionCells.get(4).getTextContent();
-                            sectionJson = new JSONObject();
-
-                            sectionJson.put("select", (sectionCells.get(0).getTextContent().equals("C") ? "C" : "SR"));
-                            sectionJson.put("crn", sectionCells.get(1).getTextContent());
-                            sectionJson.put("subject", sectionCells.get(2).getTextContent());
-                            sectionJson.put("course", sectionCells.get(3).getTextContent());
-                            sectionJson.put("section", section);
-                            sectionJson.put("campus", sectionCells.get(5).getTextContent());
-                            sectionJson.put("credits", sectionCells.get(6).getTextContent());
-                            sectionJson.put("title", sectionCells.get(7).getTextContent().substring(0, sectionCells.get(7).getTextContent().indexOf('\n')));
-                            sectionJson.put("capacity", sectionCells.get(10).getTextContent());
-                            sectionJson.put("active", sectionCells.get(11).getTextContent());
-                            sectionJson.put("remaining", sectionCells.get(12).getTextContent());
-
-                            classes = 1;
-                        }
-                        sectionJson.append("days", sectionCells.get(8).getTextContent());
-                        sectionJson.append("time", sectionCells.get(9).getTextContent());
-                        sectionJson.append("instructor", sectionCells.get(13).getTextContent().trim());
-                        sectionJson.append("date (mm/dd)", sectionCells.get(14).getTextContent());
-                        sectionJson.append("location", sectionCells.get(15).getTextContent());
-
-                        sectionJson.put("classes", classes++);
-
-                        sectionsJson.put(section, sectionJson);
-                    }
-                    courseJson.put("sections", sectionsJson);
-                    courseJson.put("title", courseRow.getCells().get(2).getTextContent());
-
-                    coursesJson.put(courseRow.getCells().get(1).getTextContent(), courseJson);
+                    specificCourses.put(courseJson);
+                    courses.put(courseJson);
                 }
 
-                subjectJson.put("courses", coursesJson);
-                subjectJson.put("title", subjectOption.getText());
-                subjectJson.put("updated", new SimpleDateFormat("EEE, MMM d yyyy 'at' hh:mm aa").format(new Date()));
-
-                try (FileWriter file = new FileWriter("data/subjects/" + subject + ".json")) {
-                    file.write(subjectJson.toString());
+                try (FileWriter file = new FileWriter("data/listings/courses/" + subjectOption.getValueAttribute() + ".json")) {
+                    file.write(specificCourses.toString());
                 }
-                subjects.put(subject);
+                WebRequest requestSettings2 = new WebRequest(
+                        new URL("https://aggie-scheduler.mybluemix.net/api/courses"), HttpMethod.POST);
+                requestSettings2.setRequestParameters(new ArrayList());
+                requestSettings2.getRequestParameters().add(new NameValuePair("term", term));
+                requestSettings2.getRequestParameters().add(new NameValuePair("courses", specificCourses.toString()));
+                Page page2 = webClient.getPage(requestSettings2);
             }
-            try (FileWriter file = new FileWriter("data/subjects.json")) {
+            try (FileWriter file = new FileWriter("data/listings/courses.json")) {
+                file.write(courses.toString());
+            }
+
+            try (FileWriter file = new FileWriter("data/listings/subjects.json")) {
                 file.write(subjects.toString());
             }
+            WebRequest requestSettings3 = new WebRequest(
+                    new URL("https://aggie-scheduler.mybluemix.net/api/subjects"), HttpMethod.POST);
+            requestSettings3.setRequestParameters(new ArrayList());
+            requestSettings3.getRequestParameters().add(new NameValuePair("term", term));
+            requestSettings3.getRequestParameters().add(new NameValuePair("subjects", subjects.toString()));
+            Page page3 = webClient.getPage(requestSettings3);
         }
     }
 }
--- src/main/java/tools/GradeHistoryScraper.java
+++ src/main/java/tools/GradeHistoryScraper.java
@@ -1,4 +1,94 @@
 package tools;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.*;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.json.JSONArray;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class GradeHistoryScraper {
+    public static void main(String[] args) throws Exception {
+        try (final WebClient webClient = new WebClient()) {
+            webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+            webClient.getOptions().setThrowExceptionOnScriptError(false);
+
+            HtmlPage page = webClient.getPage("http://web-as.tamu.edu/gradereport/");
+
+            HtmlSelect yearSelect = (HtmlSelect) page.getElementById("ctl00_plcMain_lstGradYear");
+            HtmlSelect collegeSelect = (HtmlSelect) page.getElementById("ctl00_plcMain_lstGradCollege");
+
+            JSONArray subjects = new JSONArray();
+
+            for (HtmlOption yearOption : yearSelect.getOptions()) {
+                int year = Integer.parseInt(yearOption.getValueAttribute());
+                if (year >= 2009) {
+                    for (int semester = 1; semester <= 3; semester++) {
+                        for (HtmlOption collegeOption : collegeSelect.getOptions()) {
+                            String college = collegeOption.getValueAttribute();
+                            String pdfFile = "http://web-as.tamu.edu/gradereport/PDFReports/" + year + semester + "/grd" + year + semester + college + ".pdf";
+                            try {
+                                String txtFile = downloadTxt(pdfFile);
+                                System.out.println(pdfFile + " > " + txtFile);
+                                ArrayList<String> departments = makeJson(txtFile);
+                                departments.forEach(subjects::put);
+                            } catch (Exception e) {
+                                System.out.println(pdfFile + " > not found");
+//                            e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }
+            try (FileWriter file = new FileWriter("data/subjects.json")) {
+                file.write(subjects.toString());
+            }
+        }
+    }
+
+    /*
+     * Extracts the text from the pdf link provided and saves it to a .txt file
+     * Returns the filepath of the .txt file that was generated
+     */
+    private static String downloadTxt(String pdfFile) throws Exception {
+        Writer output = null;
+        PDDocument document = null;
+        String txtFile;
+        try {
+            URL url = new URL(pdfFile);
+            document = PDDocument.load(url.openConnection().getInputStream());
+            String fileName = url.getFile();
+            File outputFile = new File(fileName.substring(0, fileName.length() - 4) + ".txt");
+            txtFile = "data/grades/raw/" + outputFile.getName();
+            output = new OutputStreamWriter(new FileOutputStream(txtFile), "UTF-8");
+            PDFTextStripper stripper = new PDFTextStripper();
+            stripper.setStartPage(1);
+            stripper.setEndPage(document.getNumberOfPages());
+            stripper.writeText(document, output);
+        } finally {
+            try {
+                if (output != null)
+                    output.close();
+                if (document != null)
+                    document.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return txtFile;
+    }
+
+    /*
+     * Given a filepath to a .txt file, extracts the information and saves it in .json format
+     * Returns an ArrayList of all departments that appeared in the file
+     */
+    private static ArrayList<String> makeJson(String txtFile) {
+        ArrayList<String> departments = new ArrayList<>();
+        return departments;
+    }
 }
+
+
